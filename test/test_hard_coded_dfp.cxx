edd67341a4638d3ec22b7fe281c6c9df82fe3488
Brett Viren <bv@bnl.gov>
2015-11-10 10:30:27 -0500

    Up through duction using hard coded type knowledge.

--- test/test_hard_coded_dfp.cxx
+++ test/test_hard_coded_dfp.cxx
@@ -12,10 +12,17 @@
 #include "WireCellUtil/Testing.h"
 #include "WireCellUtil/Units.h"
 
-// cheating
+// CHEATING.  Here is why the test is called "hard coded".  We do not
+// want to have to know about these types but instead be driven by
+// user configuration and work with base classes to build up the DFP
+// graph.
+#include "WireCellIface/IWireSelectors.h"
 #include "WireCellGen/TrackDepos.h"
 #include "WireCellGen/Drifter.h"
+#include "WireCellGen/Diffuser.h"
+#include "WireCellGen/PlaneDuctor.h"
 
+#include <sstream>
 #include <tbb/flow_graph.h>
 
 namespace dfp = tbb::flow;
@@ -27,6 +34,9 @@ using namespace std;
 template<typename OutputType>
 class TbbSourceAdapter {
 public:
+    // for debugging. counts: (N_out, N_outerr)
+    typedef std::vector<int> counter_type;
+
     // node type
     typedef ISendingNode<OutputType> sender_type;
     typedef std::shared_ptr< sender_type > sender_pointer;
@@ -34,25 +44,48 @@ public:
     // data type
     typedef std::shared_ptr<const OutputType> output_pointer;
 
-    TbbSourceAdapter( const TbbSourceAdapter& other ) : m_node(other.m_node) {}
-    TbbSourceAdapter( const sender_pointer& node ) : m_node(node) {}
-    TbbSourceAdapter( const INode::pointer& inode ) : m_node(dynamic_pointer_cast<sender_type>(inode)) {}
+    // copy ctor
+    TbbSourceAdapter( const TbbSourceAdapter& other ) : m_node(other.m_node), counter(other.counter) {}
+
+    TbbSourceAdapter( const sender_pointer& node ) : m_node(node), counter(new counter_type(2)) {}
+    TbbSourceAdapter( const INode::pointer& inode ) : m_node(dynamic_pointer_cast<sender_type>(inode)), counter(new counter_type(2)) {}
 
     ~TbbSourceAdapter() { }
     void operator=( const TbbSourceAdapter& other ) { m_node = other.m_node; }
     
     bool operator()(output_pointer& out) {
 	Assert(m_node);
-	return m_node->extract(out);
+	bool ok = m_node->extract(out);
+	if (ok) {
+	    ++ counter->at(0);
+	}
+	else {
+	    ++ counter->at(1);
+	}
+	return ok;
     }
+
+    std::string chirp() {
+	stringstream msg;
+	msg << "counts:"
+	    << " outok:" << counter->at(0)
+	    << " outerr:" << counter->at(1)
+	    << " --> " << typeid(OutputType).name() << "\n";
+	return msg.str();
+    }
+
 
 private:
     sender_pointer m_node;
+    std::shared_ptr< counter_type > counter; // shared to survive copy 
 };    
 
 template<typename InputType, typename OutputType>
 class TbbConverterAdapter {
 public:
+    // for debugging. counts: (N_in, N_out, N_inerr, N_outerr)
+    typedef std::vector<int> counter_type;
+
     typedef IConverterNode<InputType,OutputType> converter_type;
     typedef std::shared_ptr< converter_type > converter_pointer;
 
@@ -64,24 +97,30 @@ public:
 				    dfp::tuple<std::shared_ptr<const OutputType> > > multi_node;
     typedef typename multi_node::output_ports_type output_ports_type;
 
-    TbbConverterAdapter(const TbbConverterAdapter& other) : m_node(other.m_node) { }
-    TbbConverterAdapter(const converter_pointer& node) : m_node(node) { }
-    TbbConverterAdapter(const INode::pointer& inode) : m_node(dynamic_pointer_cast<converter_type>(inode)) { }
+    // copy ctor
+    TbbConverterAdapter(const TbbConverterAdapter& other) : m_node(other.m_node), counter(other.counter) { }
+    TbbConverterAdapter(const converter_pointer& node) : m_node(node), counter(new counter_type(4)) { }
+    TbbConverterAdapter(const INode::pointer& inode) : m_node(dynamic_pointer_cast<converter_type>(inode)), counter(new counter_type(4)) { }
     ~TbbConverterAdapter() { }
     void operator=( const TbbConverterAdapter& other ) { m_node = other.m_node; }
 
     void operator()(const input_pointer& in, output_ports_type& out) {
 	bool ok = m_node->insert(in);
-	if (!ok) {
-	    cerr << "Failed to insert\n";
-	    // what do?
+	if (ok) {
+	    ++ counter->at(0);
+	}
+	else {
+	    ++ counter->at(2);
 	}
 
 	while (true) {
 	    output_pointer o;
 	    ok = m_node->extract(o);
-	    if (!ok) {
-		cerr << "Failed to extract\n";
+	    if (ok) {
+		++ counter->at(1);
+	    }
+	    else {
+		++ counter->at(3);
 		break;
 	    }
 	    std::get<0>(out).try_put(o); // errors?
@@ -89,18 +128,32 @@ public:
 	return;
     }
 
+    std::string chirp() {
+	stringstream msg;
+	msg << "counts:"
+	    << " inok:" << counter->at(0)
+	    << " outok:" << counter->at(1)
+	    << " inerr:" << counter->at(2)
+	    << " outerr:" << counter->at(3)
+	    << " " << typeid(InputType).name() << " --> " << typeid(OutputType).name() << "\n";
+	return msg.str();
+    }
+
+    static multi_node adapt(dfp::graph& graph, INode::pointer wc_node, int concurrency=1) {
+	TbbConverterAdapter<InputType,OutputType> me(wc_node);
+	return multi_node(graph, concurrency, me);
+    }
+
+
+
 private:
     converter_pointer m_node;
-
+    std::shared_ptr< counter_type > counter; // shared to survive copy 
 };
 
 
+// CHEATING.
 INode::pointer make_depo() {
-    /// Here I cheat by directly making a TrackDepos.  Eventually,
-    /// this would be replaced by using NamedFactor to get a
-    /// TrackDepos object by class name and all the add_track() would
-    /// be performed by configuration.
-
     TrackDepos* td = new TrackDepos;
 
     const double cm = units::cm;
@@ -113,47 +166,70 @@ INode::pointer make_depo() {
     td->add_track(30*usec, Ray(Point(cm,-cm,-cm), Point(cm,cm,cm)));
     td->add_track(10*usec, same_point);
     td->add_track(100*usec, same_point);
+
     //td->add_track(1000*usec, same_point);
     //td->add_track(10000*usec, same_point);
 
-    cerr << "Generated " << td->depos().size() << " depositions\n";
+    cerr << "Generated " << td->depos().size() << " depositions (not counting EOS)\n";
 
     return INode::pointer(td);
 }
 
+// CHEATING.
 INode::pointer make_drifter(double to_x)
 {
     return INode::pointer(new Drifter(to_x));
 }
 
-struct DepoChirp {
-    std::string msg;
+// CHEATING.
+INode::pointer make_diffusion(const Ray& pitch, double tick, double start_time, double time_offset)
+{
+    return INode::pointer(new Diffuser(pitch, tick, time_offset, start_time));
+}
 
-    typedef std::pair<int,int> counter_type;
-    std::shared_ptr< counter_type > counter; // shared to survive copy 
+// CHEATING
+INode::pointer make_ductor(const Ray& pitch,
+				  WirePlaneLayer_t layer,
+				  const IWire::shared_vector& wires,
+				  double tick, double t0=0.0)
+{
+    WirePlaneId wpid(layer);
 
-    DepoChirp(const std::string& msg) : msg(msg), counter(new counter_type) {}
-    DepoChirp(const DepoChirp& other) : msg(other.msg), counter(other.counter) {}
-    void operator=( const DepoChirp& other ) { msg = other.msg; counter = other.counter; }
+    const double pitch_distance = ray_length(pitch);
+    const Vector pitch_unit = ray_unit(pitch);
 
-    IDepo::pointer operator()(const IDepo::pointer& depo) {
-	++counter->first;
-	cerr << msg;
-	if (depo) {
-	    cerr << ": depo: @" << depo->time() << " " << depo->pos() << endl;
-	}
-	else {
-	    cerr << ": depo: NULL\n";
-	    ++counter->second;
-	}
-	return depo;
-    }
-};
+    // get this planes wires sorted by index
+    IWire::vector plane_wires;
+    std::copy_if(wires->begin(), wires->end(),
+		 back_inserter(plane_wires), select_uvw_wires[wpid.index()]);
+    std::sort(plane_wires.begin(), plane_wires.end(), ascending_index);
+
+    // number of wires and location of wire zero measured in pitch coordinate.
+    const int nwires = plane_wires.size();
+    IWire::pointer wire_zero = plane_wires[0];
+    const Point to_wire = wire_zero->center() - pitch.first;
+    const double wire_zero_dist = pitch_unit.dot(to_wire);
+
+    // cerr << "Wire0 for plane=" << wpid.ident() << " distance=" << pitch_distance <<  " index=" << wire_zero->index() << endl;
+    // cerr << "\twire ray=" << wire_zero->ray() << endl;
+    // cerr << "\twire0 center=" << wire_zero->center() << endl;
+    // cerr << "\tto wire=" << to_wire << endl;
+    // cerr << "\tpitch = " << pitch_unit << endl;
+    // cerr << "\twire0 dist=" << wire_zero_dist << endl;
+
+    return INode::pointer(new PlaneDuctor(wpid, nwires, tick, pitch_distance, t0, wire_zero_dist));
+}
+
 
 
 int main () {
     PluginManager& pm = PluginManager::instance();
     pm.add("WireCellGen");
+
+    // should be taken from global configuration
+    const double tick = 2.0*units::microsecond; 
+    const double start_time = 0.0*units::microsecond;
+    const double drift_velocity = 1.6*units::millimeter/units::microsecond;
 
     auto wp_wps = WireCell::Factory::lookup<IWireParameters>("WireParams");
     auto pw_gen = WireCell::Factory::lookup<IWireGenerator>("WireGenerator");
@@ -167,65 +243,88 @@ int main () {
     ICellMaker::output_pointer cells;
     Assert(bc->extract(cells));
 
-    INode::pointer td = make_depo();
-    Assert(td);
-    cerr << "Got TrackDepo as INode" << endl;
-    // auto p_ids = dynamic_pointer_cast< IDepoSource > (td);
-    // Assert(p_ids);
-    // cerr << "Got TrackDepo as IDepoSource" << endl;
-    // auto p_isn = dynamic_pointer_cast< ISendingNode<IDepo> >(td);
-    // Assert(p_isn);
-    // cerr << "Got TrackDepo as ISendingNode<IDepo>" << endl;
-    TbbSourceAdapter<IDepo> td_adapt(td);
-    
+    // CHEATING.  We want to erase any explicit use of a nodes's input/output types.
     typedef TbbConverterAdapter<IDepo, IDepo> DepoDepoAdapt;
+    typedef TbbConverterAdapter<IDepo, IDiffusion> DepoDiffusionAdapt;
+    typedef TbbConverterAdapter<IDiffusion, IPlaneSlice> DiffusionPlaneSliceAdapt;
 
-    INode::pointer du = make_drifter(wp_wps->pitchU().first.x());
-    DepoDepoAdapt du_adapt(du);
-    INode::pointer dv = make_drifter(wp_wps->pitchV().first.x());
-    DepoDepoAdapt dv_adapt(dv);
-    INode::pointer dw = make_drifter(wp_wps->pitchW().first.x());
-    DepoDepoAdapt dw_adapt(dw);
-
+    // the data flow graph
     dfp::graph graph;
 
-    dfp::source_node<IDepo::pointer> td_dfp(graph, td_adapt);
+    // deposit
+    INode::pointer td = make_depo();
+    Assert(td);
+    TbbSourceAdapter<IDepo> depo_adapt(td);
+    dfp::source_node<IDepo::pointer> depo_source(graph, depo_adapt);
     
-    DepoDepoAdapt::multi_node du_dfp(graph,1,du_adapt);
-    DepoDepoAdapt::multi_node dv_dfp(graph,1,dv_adapt);
-    DepoDepoAdapt::multi_node dw_dfp(graph,1,dw_adapt);
+    // distribute deposition directly
+    dfp::broadcast_node<IDepo::pointer> fanout_depo(graph);
 
-    typedef dfp::function_node<IDepo::pointer, IDepo::pointer> depo_func;
-    
-    DepoChirp depo_chirp_d("D");
-    depo_func chirp_d(graph, 1, depo_chirp_d);
-    DepoChirp depo_chirp_u("U");
-    depo_func chirp_u(graph, 1, depo_chirp_u);
-    DepoChirp depo_chirp_v("V");
-    depo_func chirp_v(graph, 1, depo_chirp_v);
-    DepoChirp depo_chirp_w("W");
-    depo_func chirp_w(graph, 1, depo_chirp_w);
+    // drift depositions duplicitously 
+    auto drift_u_adapt = DepoDepoAdapt(make_drifter(wp_wps->pitchU().first.x()));
+    auto drift_v_adapt = DepoDepoAdapt(make_drifter(wp_wps->pitchV().first.x()));
+    auto drift_w_adapt = DepoDepoAdapt(make_drifter(wp_wps->pitchW().first.x()));
+    auto drift_u_node = DepoDepoAdapt::multi_node(graph, 1, drift_u_adapt);
+    auto drift_v_node = DepoDepoAdapt::multi_node(graph, 1, drift_v_adapt);
+    auto drift_w_node = DepoDepoAdapt::multi_node(graph, 1, drift_w_adapt);
 
-    dfp::broadcast_node<IDepo::pointer> fanout_dfp(graph);
 
-    make_edge(td_dfp, chirp_d);
-    make_edge(chirp_d, fanout_dfp);
-    make_edge(fanout_dfp, du_dfp);
-    make_edge(fanout_dfp, dv_dfp);
-    make_edge(fanout_dfp, dw_dfp);
-    make_edge(dfp::output_port<0>(du_dfp), chirp_u);
-    make_edge(dfp::output_port<0>(dv_dfp), chirp_v);
-    make_edge(dfp::output_port<0>(dw_dfp), chirp_w);
+    // diffuse drifted depositions delicately
+    auto diff_u_adapt = DepoDiffusionAdapt(make_diffusion(wp_wps->pitchU(), tick, start_time,
+							  wp_wps->pitchU().first.x()/drift_velocity));
+    auto diff_v_adapt = DepoDiffusionAdapt(make_diffusion(wp_wps->pitchV(), tick, start_time,
+							  wp_wps->pitchV().first.x()/drift_velocity));
+    auto diff_w_adapt = DepoDiffusionAdapt(make_diffusion(wp_wps->pitchW(), tick, start_time,
+							  wp_wps->pitchW().first.x()/drift_velocity));
+    auto diff_u_node = DepoDiffusionAdapt::multi_node(graph, 1, diff_u_adapt);
+    auto diff_v_node = DepoDiffusionAdapt::multi_node(graph, 1, diff_v_adapt);
+    auto diff_w_node = DepoDiffusionAdapt::multi_node(graph, 1, diff_w_adapt);
 
-    td_dfp.activate();
+
+    // ductor (con/in) digitizes diffusions deftly.
+    auto duct_u_adapt = DiffusionPlaneSliceAdapt(make_ductor(wp_wps->pitchU(), kUlayer, wires, tick, start_time));
+    auto duct_v_adapt = DiffusionPlaneSliceAdapt(make_ductor(wp_wps->pitchV(), kVlayer, wires, tick, start_time));
+    auto duct_w_adapt = DiffusionPlaneSliceAdapt(make_ductor(wp_wps->pitchW(), kWlayer, wires, tick, start_time));
+    auto duct_u_node = DiffusionPlaneSliceAdapt::multi_node(graph, 1, duct_u_adapt);
+    auto duct_v_node = DiffusionPlaneSliceAdapt::multi_node(graph, 1, duct_v_adapt);
+    auto duct_w_node = DiffusionPlaneSliceAdapt::multi_node(graph, 1, duct_w_adapt);
+
+    // now knot nodes:
+    make_edge(depo_source, fanout_depo);
+
+    make_edge(fanout_depo, drift_u_node);
+    make_edge(fanout_depo, drift_v_node);
+    make_edge(fanout_depo, drift_w_node);
+
+    make_edge(dfp::output_port<0>(drift_u_node), diff_u_node);
+    make_edge(dfp::output_port<0>(drift_v_node), diff_v_node);
+    make_edge(dfp::output_port<0>(drift_w_node), diff_w_node);
+
+    make_edge(dfp::output_port<0>(diff_u_node), duct_u_node);
+    make_edge(dfp::output_port<0>(diff_v_node), duct_v_node);
+    make_edge(dfp::output_port<0>(diff_w_node), duct_w_node);
+
+
+    // flow, Morpheus, flow
+    depo_source.activate();
     graph.wait_for_all();
 
-    cerr << "Counts:\n"
-	 << "\t#depo =" << depo_chirp_d.counter->first << "(null=" << depo_chirp_d.counter->second << ")\n"
-	 << "\t#depoU=" << depo_chirp_u.counter->first << "(null=" << depo_chirp_u.counter->second << ")\n"
-	 << "\t#depoV=" << depo_chirp_v.counter->first << "(null=" << depo_chirp_v.counter->second << ")\n"
-	 << "\t#depoW=" << depo_chirp_w.counter->first << "(null=" << depo_chirp_w.counter->second << ")\n";
-	
+
+    cerr << "Summary:\n"
+	 << "Depositions: " << depo_adapt.chirp()
+	 << "Drifted:\n"
+	 << "\tU:" << drift_u_adapt.chirp()
+	 << "\tV:" << drift_v_adapt.chirp()
+	 << "\tW:" << drift_w_adapt.chirp()
+	 << "Diffused:\n"
+	 << "\tU:" << diff_u_adapt.chirp()
+	 << "\tV:" << diff_v_adapt.chirp()
+	 << "\tW:" << diff_w_adapt.chirp()
+	 << "Ducted:\n"
+	 << "\tU:" << duct_u_adapt.chirp()
+	 << "\tV:" << duct_v_adapt.chirp()
+	 << "\tW:" << duct_w_adapt.chirp()
+	;
 
     return 0;
 }
